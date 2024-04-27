cbfa5f70c072ad532d11e49f93517168ceda6066
Alexandre Lamberty <alexandrelamberty@gmail.com>
2024-04-27 10:42:56 +0200

    fix: duration not days

--- pkg/domain/limit.go
+++ pkg/domain/limit.go
@@ -4,6 +4,6 @@ type Limit struct {
 	Entity
 	Name    string `gorm:"unique;not null" json:"name"`
 	Peoples uint   `gorm:"not null"`
-	Days    uint   `gorm:"not null"`
+	Duration    uint   `gorm:"not null"`
 	Amount  uint   `gorm:"not null"`
 }
